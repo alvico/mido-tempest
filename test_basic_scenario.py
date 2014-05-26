d23a4ca3d58c23eec251ebcc051026f43787aefe
Albert <albert.vico@midokura.com>
2014-05-26 15:30:19 +0200

    update

--- test_basic_scenario.py
+++ test_basic_scenario.py
@@ -194,9 +194,9 @@ class TestBasicScenario(manager.NetworkScenarioTest):
         ports_list = self.network_client.list_ports()
         for port in ports_list['ports']:
             for server in self.servers:
-                if port.fixedips['ip_address'] == server.ip:
+                if port['fixedips']['ip_address'] == server.ip:
                     self.network_client.update_network(port.id, {'port': {'admin_state_up': False}})
-                    self._do_test_vm_connectivity_admin_state_up(port)
+                    self._do_test_vm_connectivity_admin_state_up()
                     self.network_client.update_network(port.id, {'port': {'admin_state_up': True}})
 
     def _check_public_network_connectivity(self):
